69c5d04bb3c541f8fb02cedbb2eaea943c08d0c6
saul <saul@example.com>
2026-08-12 13:29:25 +0200

    added logger_ble

--- logger/logger/build/Desktop_Qt_6_5_3_GCC_64bit-Debug/moc_mainwindow.cpp
+++ logger/logger/build/Desktop_Qt_6_5_3_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../mainwindow.h"
+#include "../../../../logger_ble/logger/mainwindow.h"
 #include <QtCore/qmetatype.h>
 
 #if __has_include(<QtCore/qtmochelpers.h>)
@@ -45,28 +45,31 @@ static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers:
     "BTConnected",
     "BTrxData",
     "data",
-    "WiFiRead",
-    "WiFiDisplayError",
-    "QAbstractSocket::SocketError",
-    "socketError",
+    "BLEdeviceFound",
+    "device",
+    "BLErxData",
+    "BLEupdatestatus",
+    "QString&",
+    "status",
     "PlotRx",
     "PlotTimeout",
     "on_pushButtonBTdiscoverDevices_clicked",
     "on_pushButtonBTconnect_clicked",
     "on_pushButtonATSend_clicked",
-    "on_radioButtonWiFi_toggled",
-    "checked",
     "on_radioButtonBT_toggled",
-    "on_pushButtonWiFiConnect_clicked",
-    "on_pushButtonWiFiDisconnect_clicked",
+    "checked",
     "on_checkBoxConfigAntialias_toggled",
     "on_pushButtonBTdisconnect_clicked",
     "on_actionStart_triggered",
-    "on_actionStop_triggered"
+    "on_actionStop_triggered",
+    "on_radioButtonBLE_toggled",
+    "on_pushButtonBLEDiscover_clicked",
+    "on_pushButtonBLEConnect_clicked",
+    "on_pushButtonBLEDisconnect_clicked"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[50];
+    uint offsetsAndSizes[56];
     char stringdata0[11];
     char stringdata1[12];
     char stringdata2[1];
@@ -74,24 +77,27 @@ struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
     char stringdata4[12];
     char stringdata5[9];
     char stringdata6[5];
-    char stringdata7[9];
-    char stringdata8[17];
-    char stringdata9[29];
-    char stringdata10[12];
-    char stringdata11[7];
-    char stringdata12[12];
-    char stringdata13[39];
-    char stringdata14[31];
-    char stringdata15[28];
-    char stringdata16[27];
-    char stringdata17[8];
+    char stringdata7[15];
+    char stringdata8[7];
+    char stringdata9[10];
+    char stringdata10[16];
+    char stringdata11[9];
+    char stringdata12[7];
+    char stringdata13[7];
+    char stringdata14[12];
+    char stringdata15[39];
+    char stringdata16[31];
+    char stringdata17[28];
     char stringdata18[25];
-    char stringdata19[33];
-    char stringdata20[36];
-    char stringdata21[35];
-    char stringdata22[34];
-    char stringdata23[25];
-    char stringdata24[24];
+    char stringdata19[8];
+    char stringdata20[35];
+    char stringdata21[34];
+    char stringdata22[25];
+    char stringdata23[24];
+    char stringdata24[26];
+    char stringdata25[33];
+    char stringdata26[32];
+    char stringdata27[35];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -104,24 +110,27 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
         QT_MOC_LITERAL(29, 11),  // "BTConnected"
         QT_MOC_LITERAL(41, 8),  // "BTrxData"
         QT_MOC_LITERAL(50, 4),  // "data"
-        QT_MOC_LITERAL(55, 8),  // "WiFiRead"
-        QT_MOC_LITERAL(64, 16),  // "WiFiDisplayError"
-        QT_MOC_LITERAL(81, 28),  // "QAbstractSocket::SocketError"
-        QT_MOC_LITERAL(110, 11),  // "socketError"
-        QT_MOC_LITERAL(122, 6),  // "PlotRx"
-        QT_MOC_LITERAL(129, 11),  // "PlotTimeout"
-        QT_MOC_LITERAL(141, 38),  // "on_pushButtonBTdiscoverDevice..."
-        QT_MOC_LITERAL(180, 30),  // "on_pushButtonBTconnect_clicked"
-        QT_MOC_LITERAL(211, 27),  // "on_pushButtonATSend_clicked"
-        QT_MOC_LITERAL(239, 26),  // "on_radioButtonWiFi_toggled"
-        QT_MOC_LITERAL(266, 7),  // "checked"
-        QT_MOC_LITERAL(274, 24),  // "on_radioButtonBT_toggled"
-        QT_MOC_LITERAL(299, 32),  // "on_pushButtonWiFiConnect_clicked"
-        QT_MOC_LITERAL(332, 35),  // "on_pushButtonWiFiDisconnect_c..."
-        QT_MOC_LITERAL(368, 34),  // "on_checkBoxConfigAntialias_to..."
-        QT_MOC_LITERAL(403, 33),  // "on_pushButtonBTdisconnect_cli..."
-        QT_MOC_LITERAL(437, 24),  // "on_actionStart_triggered"
-        QT_MOC_LITERAL(462, 23)   // "on_actionStop_triggered"
+        QT_MOC_LITERAL(55, 14),  // "BLEdeviceFound"
+        QT_MOC_LITERAL(70, 6),  // "device"
+        QT_MOC_LITERAL(77, 9),  // "BLErxData"
+        QT_MOC_LITERAL(87, 15),  // "BLEupdatestatus"
+        QT_MOC_LITERAL(103, 8),  // "QString&"
+        QT_MOC_LITERAL(112, 6),  // "status"
+        QT_MOC_LITERAL(119, 6),  // "PlotRx"
+        QT_MOC_LITERAL(126, 11),  // "PlotTimeout"
+        QT_MOC_LITERAL(138, 38),  // "on_pushButtonBTdiscoverDevice..."
+        QT_MOC_LITERAL(177, 30),  // "on_pushButtonBTconnect_clicked"
+        QT_MOC_LITERAL(208, 27),  // "on_pushButtonATSend_clicked"
+        QT_MOC_LITERAL(236, 24),  // "on_radioButtonBT_toggled"
+        QT_MOC_LITERAL(261, 7),  // "checked"
+        QT_MOC_LITERAL(269, 34),  // "on_checkBoxConfigAntialias_to..."
+        QT_MOC_LITERAL(304, 33),  // "on_pushButtonBTdisconnect_cli..."
+        QT_MOC_LITERAL(338, 24),  // "on_actionStart_triggered"
+        QT_MOC_LITERAL(363, 23),  // "on_actionStop_triggered"
+        QT_MOC_LITERAL(387, 25),  // "on_radioButtonBLE_toggled"
+        QT_MOC_LITERAL(413, 32),  // "on_pushButtonBLEDiscover_clicked"
+        QT_MOC_LITERAL(446, 31),  // "on_pushButtonBLEConnect_clicked"
+        QT_MOC_LITERAL(478, 34)   // "on_pushButtonBLEDisconnect_cl..."
     },
     "MainWindow",
     "BTgetDevice",
@@ -130,24 +139,27 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
     "BTConnected",
     "BTrxData",
     "data",
-    "WiFiRead",
-    "WiFiDisplayError",
-    "QAbstractSocket::SocketError",
-    "socketError",
+    "BLEdeviceFound",
+    "device",
+    "BLErxData",
+    "BLEupdatestatus",
+    "QString&",
+    "status",
     "PlotRx",
     "PlotTimeout",
     "on_pushButtonBTdiscoverDevices_clicked",
     "on_pushButtonBTconnect_clicked",
     "on_pushButtonATSend_clicked",
-    "on_radioButtonWiFi_toggled",
-    "checked",
     "on_radioButtonBT_toggled",
-    "on_pushButtonWiFiConnect_clicked",
-    "on_pushButtonWiFiDisconnect_clicked",
+    "checked",
     "on_checkBoxConfigAntialias_toggled",
     "on_pushButtonBTdisconnect_clicked",
     "on_actionStart_triggered",
-    "on_actionStop_triggered"
+    "on_actionStop_triggered",
+    "on_radioButtonBLE_toggled",
+    "on_pushButtonBLEDiscover_clicked",
+    "on_pushButtonBLEConnect_clicked",
+    "on_pushButtonBLEDisconnect_clicked"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -159,7 +171,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-      18,   14, // methods
+      20,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -167,41 +179,45 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    1,  122,    2, 0x08,    1 /* Private */,
-       4,    1,  125,    2, 0x08,    3 /* Private */,
-       5,    1,  128,    2, 0x08,    5 /* Private */,
-       7,    0,  131,    2, 0x08,    7 /* Private */,
-       8,    1,  132,    2, 0x08,    8 /* Private */,
-      11,    1,  135,    2, 0x08,   10 /* Private */,
-      12,    0,  138,    2, 0x08,   12 /* Private */,
-      13,    0,  139,    2, 0x08,   13 /* Private */,
-      14,    0,  140,    2, 0x08,   14 /* Private */,
-      15,    0,  141,    2, 0x08,   15 /* Private */,
-      16,    1,  142,    2, 0x08,   16 /* Private */,
-      18,    1,  145,    2, 0x08,   18 /* Private */,
-      19,    0,  148,    2, 0x08,   20 /* Private */,
-      20,    0,  149,    2, 0x08,   21 /* Private */,
-      21,    1,  150,    2, 0x08,   22 /* Private */,
-      22,    0,  153,    2, 0x08,   24 /* Private */,
-      23,    0,  154,    2, 0x08,   25 /* Private */,
-      24,    0,  155,    2, 0x08,   26 /* Private */,
+       1,    1,  134,    2, 0x08,    1 /* Private */,
+       4,    1,  137,    2, 0x08,    3 /* Private */,
+       5,    1,  140,    2, 0x08,    5 /* Private */,
+       7,    1,  143,    2, 0x08,    7 /* Private */,
+       9,    1,  146,    2, 0x08,    9 /* Private */,
+      10,    1,  149,    2, 0x08,   11 /* Private */,
+      13,    1,  152,    2, 0x08,   13 /* Private */,
+      14,    0,  155,    2, 0x08,   15 /* Private */,
+      15,    0,  156,    2, 0x08,   16 /* Private */,
+      16,    0,  157,    2, 0x08,   17 /* Private */,
+      17,    0,  158,    2, 0x08,   18 /* Private */,
+      18,    1,  159,    2, 0x08,   19 /* Private */,
+      20,    1,  162,    2, 0x08,   21 /* Private */,
+      21,    0,  165,    2, 0x08,   23 /* Private */,
+      22,    0,  166,    2, 0x08,   24 /* Private */,
+      23,    0,  167,    2, 0x08,   25 /* Private */,
+      24,    1,  168,    2, 0x08,   26 /* Private */,
+      25,    0,  171,    2, 0x08,   28 /* Private */,
+      26,    0,  172,    2, 0x08,   29 /* Private */,
+      27,    0,  173,    2, 0x08,   30 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QString,    3,
     QMetaType::Void, QMetaType::QString,    3,
     QMetaType::Void, QMetaType::QByteArray,    6,
-    QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 9,   10,
+    QMetaType::Void, QMetaType::QString,    8,
+    QMetaType::Void, QMetaType::QByteArray,    6,
+    QMetaType::Void, 0x80000000 | 11,   12,
     QMetaType::Void, QMetaType::QByteArray,    6,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Bool,   17,
-    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   19,
+    QMetaType::Void, QMetaType::Bool,   19,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   19,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -227,11 +243,15 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         // method 'BTrxData'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<const QByteArray &, std::false_type>,
-        // method 'WiFiRead'
+        // method 'BLEdeviceFound'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'WiFiDisplayError'
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'BLErxData'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<QAbstractSocket::SocketError, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QByteArray &, std::false_type>,
+        // method 'BLEupdatestatus'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString &, std::false_type>,
         // method 'PlotRx'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<const QByteArray &, std::false_type>,
@@ -243,16 +263,9 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_pushButtonATSend_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_radioButtonWiFi_toggled'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
         // method 'on_radioButtonBT_toggled'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<bool, std::false_type>,
-        // method 'on_pushButtonWiFiConnect_clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_pushButtonWiFiDisconnect_clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_checkBoxConfigAntialias_toggled'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         QtPrivate::TypeAndForceComplete<bool, std::false_type>,
@@ -261,6 +274,15 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         // method 'on_actionStart_triggered'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_actionStop_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_radioButtonBLE_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_pushButtonBLEDiscover_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButtonBLEConnect_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButtonBLEDisconnect_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -275,33 +297,24 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 0: _t->BTgetDevice((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
         case 1: _t->BTConnected((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
         case 2: _t->BTrxData((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1]))); break;
-        case 3: _t->WiFiRead(); break;
-        case 4: _t->WiFiDisplayError((*reinterpret_cast< std::add_pointer_t<QAbstractSocket::SocketError>>(_a[1]))); break;
-        case 5: _t->PlotRx((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1]))); break;
-        case 6: _t->PlotTimeout(); break;
-        case 7: _t->on_pushButtonBTdiscoverDevices_clicked(); break;
-        case 8: _t->on_pushButtonBTconnect_clicked(); break;
-        case 9: _t->on_pushButtonATSend_clicked(); break;
-        case 10: _t->on_radioButtonWiFi_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 3: _t->BLEdeviceFound((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 4: _t->BLErxData((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1]))); break;
+        case 5: _t->BLEupdatestatus((*reinterpret_cast< std::add_pointer_t<QString&>>(_a[1]))); break;
+        case 6: _t->PlotRx((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1]))); break;
+        case 7: _t->PlotTimeout(); break;
+        case 8: _t->on_pushButtonBTdiscoverDevices_clicked(); break;
+        case 9: _t->on_pushButtonBTconnect_clicked(); break;
+        case 10: _t->on_pushButtonATSend_clicked(); break;
         case 11: _t->on_radioButtonBT_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
-        case 12: _t->on_pushButtonWiFiConnect_clicked(); break;
-        case 13: _t->on_pushButtonWiFiDisconnect_clicked(); break;
-        case 14: _t->on_checkBoxConfigAntialias_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
-        case 15: _t->on_pushButtonBTdisconnect_clicked(); break;
-        case 16: _t->on_actionStart_triggered(); break;
-        case 17: _t->on_actionStop_triggered(); break;
+        case 12: _t->on_checkBoxConfigAntialias_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 13: _t->on_pushButtonBTdisconnect_clicked(); break;
+        case 14: _t->on_actionStart_triggered(); break;
+        case 15: _t->on_actionStop_triggered(); break;
+        case 16: _t->on_radioButtonBLE_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 17: _t->on_pushButtonBLEDiscover_clicked(); break;
+        case 18: _t->on_pushButtonBLEConnect_clicked(); break;
+        case 19: _t->on_pushButtonBLEDisconnect_clicked(); break;
         default: ;
-        }
-    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        switch (_id) {
-        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
-        case 4:
-            switch (*reinterpret_cast<int*>(_a[1])) {
-            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
-            case 0:
-                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QAbstractSocket::SocketError >(); break;
-            }
-            break;
         }
     }
 }
@@ -325,13 +338,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 18)
+        if (_id < 20)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 18;
+        _id -= 20;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 18)
-            qt_static_metacall(this, _c, _id, _a);
-        _id -= 18;
+        if (_id < 20)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 20;
     }
     return _id;
 }
---
+++ logger_ble/logger/mainwindow.cpp
@@ -0,0 +1,364 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QtCharts/QChartView>
+
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    /**** Toolbar ******/
+       ui->mainToolBar->setIconSize(QSize(120, 120));
+
+    //config initialization, BLE set as default
+
+    ui->pushButtonBTconnect->setEnabled(false);
+    ui->pushButtonBTdiscoverDevices->setEnabled(false);
+    ui->pushButtonBTdisconnect->setEnabled(false);
+
+    ui->pushButtonBLEConnect->setEnabled(true);
+    ui->pushButtonBLEDiscover->setEnabled(true);
+    ui->pushButtonBLEDisconnect->setEnabled(false);
+
+    // Plot configuration
+
+    m_MaxDataPlot = 640;
+    m_DataCounter = 0;
+    m_MaxNumSamples = 6000; //Max Number of samples to be recorded before the buffers are cleared
+    m_PlotCounter = 0;
+    m_PlotTimeout = 100; //time in ms
+    m_PlotNumUpdate = 10; //number of samples to update every m_PlotTimeout
+
+    //Bluetooth
+    m_bt = new BTClient(this);
+    connect(m_bt, SIGNAL(BTnewDeviceDiscovered(QString)),
+            this, SLOT(BTgetDevice(QString)));
+    connect(m_bt, SIGNAL(BTnewDeviceConnected(QString)),
+            this, SLOT(BTConnected(QString)));
+    connect(m_bt, SIGNAL(BTrxData(QByteArray)),
+            this, SLOT(BTrxData(const QByteArray)));
+
+
+    //BLE
+    m_ble = new BLEClient(this);
+    connect(m_ble, &BLEClient::deviceFound, this, &MainWindow::BLEdeviceFound);
+    connect(m_ble, &BLEClient::rxData, this, &MainWindow::BLErxData);
+    connect(m_ble, &BLEClient::BLEupdateStatus, this, &MainWindow::BLEupdatestatus);
+
+    //Plot
+    m_chart = new CPlotChart();
+    m_chart->setTitles("","Time","ADC");
+    m_chart->setXMinXax(0,m_MaxDataPlot);
+    m_chart->setYMinXax(0,1024);
+    m_chart->initializePlot();
+    m_chartView = new QChartView(static_cast<QChart*>(m_chart));
+    //Set antialising properties and the chartview object to a place in layout
+    m_chartView->setRenderHint(QPainter::Antialiasing, false); //false or true
+    ui->verticalLayout_3->addWidget(m_chartView,1);
+
+    //timer to refresh the plot
+    m_timer = new QTimer(this);
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(PlotTimeout()));
+    //m_timer->start(m_PlotTimeout);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::BTgetDevice(QString name)
+{
+    qDebug()<< name;
+    ui->comboBoxBTdevices->addItem(name);
+}
+
+void MainWindow::BTConnected(QString name)
+{
+    QString message = "Connected to: " + name;
+    //ui->labelBTstatus->setText(message);
+    ui->labelBTstatus->setText("Connected");
+    qDebug()<< message;
+
+    ui->pushButtonBTdisconnect->setEnabled(true);
+}
+
+void MainWindow::BTrxData(const QByteArray &data)
+{
+    ui->plainTextEditAT->appendPlainText(QString::fromStdString(data.toStdString()));
+
+    m_data.append(data);
+
+    //Continuous sampling processing
+    if (!m_timer->isActive())
+        return;
+
+    int size = data.count();
+    if (size%2) { //each values is 2 bytes. There is an incomplete value!
+        qDebug()<<"Odd number of bytes received";
+        return; //wait for the next rx
+    } else { // Process the received values
+        PlotRx(m_data);
+        m_data.clear();
+    }
+
+}
+
+
+void MainWindow::PlotRx(const QByteArray &data)
+{
+    DataPoint aux_point;
+    int index = 0;   
+    quint16 value;
+
+    int size = data.count()/2; //Each converted value comes in 2 bytes unsigned short.
+
+    for (int i = 0; i < size; i++) {
+        //reconstruct the digital value
+        value = data.at(index++) & 0xff;
+        value |= (data.at(index++) << 8) & 0xff00;
+
+        //Create a data point
+        aux_point.first.setX(m_DataCounter);
+        aux_point.first.setY(value);
+        aux_point.second = QString::number(m_DataCounter);
+
+        //Save the data point for further processing/plotting
+        m_trace.append(aux_point);
+        m_DataCounter++;
+    }
+    //The data is plotted using a time_out
+}
+
+void MainWindow::PlotTimeout()
+{
+    int head;
+    int plot_index;
+     DataPoint aux_point;
+
+    if (m_PlotCounter == m_DataCounter) // No new samples added at this time
+        return;
+
+    //Update the plot's trace list
+    //Uncomment the following lines in order to smooth the updating ofplot (only works fine in desktop)
+   /* head = m_PlotCounter + m_PlotNumUpdate;
+    if (head > m_DataCounter)
+        head = m_DataCounter;
+   */
+
+    //Uncomment the next line in order to update the plot with all the new data at once
+    //head = m_DataCounter;
+
+    if (ui->checkBoxConfigSmoothPlot->isChecked()) { //Only add M_PlotNumUpdate samples to the plot
+        head = m_PlotCounter + m_PlotNumUpdate;
+        if (head > m_DataCounter) {
+                head = m_DataCounter;
+        }
+    } else {    //Add all new samples to the plot
+        head = m_DataCounter;
+    }
+
+    for (int i = m_PlotCounter; i < head; i++) {
+        plot_index = m_PlotCounter%m_MaxDataPlot; //create a index between 0 - m_MaxDataPot
+        if (plot_index == 0) {  //clear the plot trace
+            m_plot_trace.clear();
+        } else {
+            aux_point = m_trace.at(i);
+            aux_point.first.setX(plot_index);
+            m_plot_trace.append(aux_point);
+        }
+        m_PlotCounter++;
+    }
+
+    //replot the trace
+    if(m_plot_trace.count()) {
+        m_chart->clearTable();
+        m_chart->addTrace(m_plot_trace);
+        m_chart->updatePlot();
+    }
+
+    //Check if number of samples are exceeded
+    if (m_trace.count() >= m_MaxNumSamples) {
+        m_trace.clear();
+        m_DataCounter = 0;
+        m_plot_trace.clear();
+        m_PlotCounter = 0;
+    }
+}
+
+
+void MainWindow::on_pushButtonBTdiscoverDevices_clicked()
+{
+    ui->comboBoxBTdevices->clear();
+    m_bt->BTfindDevices();
+}
+
+void MainWindow::on_pushButtonBTconnect_clicked()
+{
+    QString name;
+    name = ui->comboBoxBTdevices->currentText();
+    m_bt->BTconnect(name);
+    ui->labelBTstatus->setText("Connecting...");
+}
+
+void MainWindow::on_pushButtonATSend_clicked()
+{
+    QString aux = ui->lineEditAT->text();
+
+    QByteArray data;
+    data.append(aux.toUtf8());
+    //data.append()
+
+    if (ui->radioButtonBT->isChecked()) {
+        m_bt->BTwrite(data);
+    } else {    //The communication with the ESP-01 module is always terminated by cr + nl
+        data.append('\r');
+        data.append('\n');
+        //m_WiFiTcpSocket->write(data);
+    }
+
+    //if continuous sampling is sent, start the timer
+    switch (data.at(0)) {
+        case 's':   m_timer->start(m_PlotTimeout);
+                    break;
+        case 'S':   m_timer->stop();
+                    break;
+        default:
+                    break;
+
+    }
+}
+
+void MainWindow::on_radioButtonBT_toggled(bool checked)
+{
+    if (checked) {
+        ui->pushButtonBTconnect->setEnabled(true);
+        ui->pushButtonBTdiscoverDevices->setEnabled(true);
+        ui->pushButtonBTdisconnect->setEnabled(false);
+
+        ui->pushButtonBLEConnect->setEnabled(false);
+        ui->pushButtonBLEDiscover->setEnabled(false);
+        ui->pushButtonBLEDisconnect->setEnabled(false);
+
+    }
+
+}
+
+void MainWindow::on_checkBoxConfigAntialias_toggled(bool checked)
+{
+    if (checked) {
+        m_chartView->setRenderHint(QPainter::Antialiasing, true); //false or true
+    } else {
+        m_chartView->setRenderHint(QPainter::Antialiasing, false); //false or true
+    }
+}
+
+void MainWindow::on_pushButtonBTdisconnect_clicked()
+{
+    m_bt->BTdisconnect();
+    ui->labelBTstatus->setText("Disconnected");
+}
+
+void MainWindow::on_actionStart_triggered()
+{
+    QByteArray data;
+    data.append("s");
+
+    if (ui->radioButtonBT->isChecked()) {
+        m_bt->BTwrite(data);
+    } else {    //The communication with the ESP-01 module is always terminated by cr + nl
+        data.append('\r');
+        data.append('\n');
+        //m_WiFiTcpSocket->write(data);
+    }
+
+    m_timer->start(m_PlotTimeout);
+
+}
+
+
+void MainWindow::on_actionStop_triggered()
+{
+    QByteArray data;
+    data.append("S");
+
+    if (ui->radioButtonBT->isChecked()) {
+        m_bt->BTwrite(data);
+    } else {    //The communication with the ESP-01 module is always terminated by cr + nl
+        data.append('\r');
+        data.append('\n');
+        //m_WiFiTcpSocket->write(data);
+    }
+
+    m_timer->stop();
+}
+
+
+void MainWindow::on_radioButtonBLE_toggled(bool checked)
+{
+    if (checked) {
+
+        ui->pushButtonBLEConnect->setEnabled(true);
+        ui->pushButtonBLEDiscover->setEnabled(true);
+        ui->pushButtonBLEDisconnect->setEnabled(false);
+
+        ui->pushButtonBTconnect->setEnabled(false);
+        ui->pushButtonBTdiscoverDevices->setEnabled(false);
+        ui->pushButtonBTdisconnect->setEnabled(false);
+    }
+}
+
+
+void MainWindow::BLEdeviceFound(QString device)
+{
+    ui->comboBoxBLE->addItem(device);
+
+}
+
+void MainWindow::BLErxData(const QByteArray &data)
+{
+    QString aux;
+    aux = QString::fromStdString(data.toStdString());
+    //ui->labelBLEStatus->setText(aux);
+    qDebug()<< aux;
+    BTrxData(data);
+}
+
+void MainWindow::BLEupdatestatus(QString &status)
+{
+    ui->labelBLEStatus->setText(status);
+
+    if (status == "Connected") {
+        ui->pushButtonBLEDisconnect->setEnabled(true);
+    }
+
+    if (status == "Disconnected") {
+        ui->pushButtonBLEDisconnect->setEnabled(false);
+    }
+}
+
+
+void MainWindow::on_pushButtonBLEDiscover_clicked()
+{
+    ui->comboBoxBLE->clear();
+    m_ble->discover();
+}
+
+
+void MainWindow::on_pushButtonBLEConnect_clicked()
+{
+    int index = ui->comboBoxBLE->currentIndex();
+    m_ble->BLEconnect(index);
+}
+
+
+void MainWindow::on_pushButtonBLEDisconnect_clicked()
+{
+    m_ble->BLEdisconnect();
+}
+
